1a413871834e4bf60823a1b504b7de3be00f65b0
Ray O <71047193+cs-cmd@users.noreply.github.com>
2024-01-22 20:03:10 +0000

    Added CORS options for local machine, port 5000 (will change when rolled into main project);

--- src/server.js
+++ src/server.js
@@ -4,10 +4,16 @@
 // gather data from
 import express from "express";
 import { createClient } from "@libsql/client";
+import cors from "cors";
 import menuItemsRouter from "./routers/menu-router.js";
 
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
+// const whitelist = ['http://localhost:5000'];
+const corsOptions = {
+  origin: "http://localhost:5000",
+  optionsSuccessStatus: 200,
+};
 
 const client = createClient({
   url: process.env.LIBSQL_CONN,
