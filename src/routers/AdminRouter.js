b0a021730b615e5fe17b45e43edd22b4139cb2e2
Ray O <71047193+cs-cmd@users.noreply.github.com>
2024-01-24 17:34:27 +0000

    Added express-validation; Edited login page to display error messages;

--- src/routers/AdminRouter.js
+++ src/routers/AdminRouter.js
@@ -1,8 +1,47 @@
-import express from 'express';
+import express from "express";
+import { body, validationResult } from "express-validator";
+import asyncHandler from "express-async-handler";
 
 const adminRouter = express.Router();
+const users = [
+  {
+    email: "johndoe@hello.com",
+    password: "helpme",
+  },
+  {
+    email: "ppaul@hello.com",
+    password: "helpdeargod",
+  },
+];
 
-adminRouter.post('/login', async (req, res, next) => {
-});
+adminRouter.post("/login", [
+  body("email", "Please enter an email")
+    .trim()
+    .notEmpty()
+    .withMessage("Please enter a valid email")
+    .bail()
+    .escape()
+    .custom(
+      asyncHandler((modifiedEmail) => {
+        console.log("modifiedEmail: ", modifiedEmail);
+        const isValidUserEmail = users.some(
+          (user) => user.email === modifiedEmail,
+        );
+        if (!isValidUserEmail) {
+          throw new Error("Email not on file, please check email and retry");
+        }
+        return true;
+      }),
+    ),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+    }
+    const { email, password } = req.body;
+    res.send("login");
+  },
+]);
 
 export default adminRouter;
