110e60a4191b03bee2c875a8084aff0c9c916ebf
Ray O <71047193+cs-cmd@users.noreply.github.com>
2024-01-24 21:29:35 +0000

    Attempted to add redirecting/rerendering based on selected button in 'browse all';

--- src/controller/browseItemsController.js
+++ src/controller/browseItemsController.js
@@ -1,10 +1,48 @@
 import express from "express";
+import { client } from "#src/server.js";
+
 const browseItemsController = express.Router();
 
-function browseItemsGetMain(req, res) {
+function browseMainMenu(req, res) {
   res.render("browse");
 }
 
-browseItemsController.get("/", browseItemsGetMain);
+function queryAllMenuItems(req, res, next) {
+  // query all items from menu_items
+  const menuItems = {
+    name: "soup",
+  };
+  next();
+}
+function browseMainMenuRedirect(req, res) {
+  const { display_items, result_type } = req.body;
+  console.log("in redirect post");
+  console.log(req.body);
+  res.render("browse", {
+    result_type,
+    display_items,
+  });
+}
+
+function queryAllUsers(req, res, next) {
+  res.render("browse", {
+    result_type: "users",
+    display_items: {
+      name: "Product",
+    },
+  });
+}
+
+function queryAllFlavors(req, res) {
+  // query all items from flavors
+}
+
+browseItemsController.get("/", browseMainMenu);
+browseItemsController.get(
+  "/menu-items",
+  queryAllMenuItems,
+  browseMainMenuRedirect,
+);
+browseItemsController.get("/users", queryAllUsers, browseMainMenuRedirect);
 
 export default browseItemsController;
