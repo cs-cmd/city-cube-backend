ba1573ebedc65cac6bd57e43f9db943d8a9bf26f
Ray O <71047193+cs-cmd@users.noreply.github.com>
2024-01-26 19:42:24 +0000

    Added email/password validation to loginPost (no sanitization yet);

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -5,10 +5,30 @@ function loginGet(req, res) {
 }
 
 const loginPost = [
-  body("email", "Email is required"),
-  body("password", "Password is required"),
-  (req, res) => {
+  (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
+
+    if (!email) {
+      req.body.error_message = "Please enter your email.";
+      next();
+    } else if (!password) {
+      req.body.error_message = "Please enter your password.";
+      next();
+    } else if (!testMenuItemsDb.checkIfUser(email)) {
+      req.body.error_message = "User does not exist.";
+      next();
+    } else if (!testMenuItemsDb.checkUserPassword(email, password)) {
+      req.body.error_message = "Incorrect password.";
+      next();
+    } else {
+      next();
+    }
+  },
+  (req, res) => {
+    const errorMessage = req.body.error_message;
+    if (errorMessage) {
+      res.render("login", { error_message: errorMessage });
+    }
   },
 ];
