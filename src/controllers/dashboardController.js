b3530fc927f845df4c5eeb28f1a6da03ec899ba1
Ray O <71047193+cs-cmd@users.noreply.github.com>
2024-01-26 19:55:26 +0000

    Added theoretical middleware to redirect user to login if not signed in (global to `dashboard`);

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,6 +1,12 @@
-// gets basic user data
-function dashboardHomeGet(req, res) {
-  res.render("dashboard");
-}
+const dashboardHomeGet = [
+  (req, res, next) => {
+    // if user not logged in, redirect to login
+    // res.redirect("/login");
+    next();
+  },
+  (req, res) => {
+    res.render("dashboard");
+  },
+];
 
 export { dashboardHomeGet };
