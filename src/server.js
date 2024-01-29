15df030b56a4e496047cf667e7b696234880e67a
Ray O <71047193+cs-cmd@users.noreply.github.com>
2024-01-29 15:14:59 +0000

    Added redirect from root path of server `/` to `/login` by default;;

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.redirect("/login");
 });
 
 app.use("/dashboard", dashboardRouter);
